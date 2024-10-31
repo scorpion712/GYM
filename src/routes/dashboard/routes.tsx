8ae8aa9e538e91d8b91eae8c1f103fdeb46286de
lautaro.diez@sbi-technology.com <lautaro.diez@sbi-technology.com>
2024-10-30 23:05:43 -0300

    Routes for users registration

--- src/routes/dashboard/routes.tsx
+++ src/routes/dashboard/routes.tsx
@@ -3,9 +3,11 @@ import { Outlet } from "react-router-dom";
 
 import { AuthGuard } from "../../guards";
 import { Layout as DashboardLayout } from '../../components/layouts/dashboard/index';
+import { paths } from "../paths";
 
 // eslint-disable-next-line react-refresh/only-export-components
 const HomePage = lazy(() => import('../../pages/home'));
+const CreateUserPage = lazy(() => import('../../pages/users/create'));
 
 export const dashboardRoutes = [
     {
@@ -23,6 +25,10 @@ export const dashboardRoutes = [
             {
                 index: true,
                 element: <HomePage />,
+            },
+            {
+                path: paths.users.create,
+                element: <CreateUserPage />,
             },
         ],
     },
