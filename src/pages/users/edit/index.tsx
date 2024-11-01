150b525dbd329021a78711ad409ce33b823a64f5
Lautaro Diez <lauutii.127@gmail.com>
2024-11-01 15:10:27 +0000

    Edit user route and page

---
+++ src/pages/users/edit/index.tsx
@@ -0,0 +1,21 @@
+import Box from '@mui/material/Box';
+
+import { CreateUserContainer, Seo } from '../../../components';
+import { Container } from '@mui/material';
+
+const Page = () => {
+    return (
+        <>
+            <Seo title="Editar Usuario" />
+            <Box
+                component="main" 
+            >
+                <Container maxWidth="xl" >
+                    <CreateUserContainer />
+                </Container>
+            </Box>
+        </>
+    );
+};
+
+export default Page;
--- src/routes/dashboard/routes.tsx
+++ src/routes/dashboard/routes.tsx
@@ -8,6 +8,7 @@ import { paths } from "../paths";
 // eslint-disable-next-line react-refresh/only-export-components
 const HomePage = lazy(() => import('../../pages/home'));
 const CreateUserPage = lazy(() => import('../../pages/users/create'));
+const EditUserPage = lazy(() => import('../../pages/users/edit'));
 
 export const dashboardRoutes = [
     {
@@ -29,6 +30,10 @@ export const dashboardRoutes = [
             {
                 path: paths.users.create,
                 element: <CreateUserPage />,
+            },
+            {
+                path: paths.users.edit,
+                element: <EditUserPage />,
             },
         ],
     },
