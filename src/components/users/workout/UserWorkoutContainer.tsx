10bc2cb62f1184e3d0b4ed630f611f59de9dd748
lautaro.diez@sbi-technology.com <lautaro.diez@sbi-technology.com>
2025-01-08 17:52:52 -0300

    User workout history container and details

--- src/components/users/workout/UserWorkoutContainer.tsx
+++ src/components/users/workout/UserWorkoutContainer.tsx
@@ -1,4 +1,4 @@
-import { Box, LinearProgress, Paper, Stack, SvgIcon, Typography } from "@mui/material";
+import { Box, Button, LinearProgress, Paper, Stack, SvgIcon, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import EventBusyIcon from '@mui/icons-material/EventBusy';
@@ -6,69 +6,37 @@ import EventAvailableIcon from '@mui/icons-material/EventAvailable';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
 import TimerIcon from '@mui/icons-material/Timer';
 
-import { useService } from "../../../hooks";
+import { useRouter, useService } from "../../../hooks";
 import { workoutService } from "../../../services";
 import { useLocation } from "react-router-dom";
-import { GetUserWorkoutResponse, UserWorkout, Workout } from "../../../models";
+import { GetUserWorkoutResponse, UserWorkout } from "../../../models";
 import { UserWorkoutWeek } from "./UserWorkoutWeek";
 import { primary } from "../../../theme/colors";
 import { es } from "date-fns/locale";
 import { format } from "date-fns";
 import { adaptGetUserWorkoutToUserWorkout } from "../../../adapters";
+import { paths } from "../../../routes/paths";
 
-const mockedUserWorkout = {
-    id: "1",
-    userId: "1",
-    userName: "Ronnie Coleman",
-    userDaysPerWeek: [true, true, true, true, true],
-    workoutPlanName: "Hipertrofia",
-    objective: "Aumento de Hipertrofia",
-    duration: 4,
-    initDate: new Date(), 
-    workouts: [
-        {
-            id: "1",
-            week: 1,
-            day: 1,
-            description: "Upper body workout",
-            exercise: [
-                { id: "1", name: "Bench Press", series: 3, repetitions: 10, weight: 70, rir: 2, rpe: 8, comments: "Felt strong" },
-                { id: "2", name: "Pull-ups", series: 3, repetitions: 8, weight: 0, rir: 1, rpe: 9, comments: "Need to improve" }
-            ]
-        },
-        {
-            id: "2",
-            week: 1,
-            day: 2,
-            description: "Lower body workout",
-            exercise: [
-                { id: "3", name: "Squats", series: 3, repetitions: 10, weight: 90, rir: 2, rpe: 8, comments: "Good form" },
-                { id: "4", name: "Leg Press", series: 3, repetitions: 10, weight: 120 },
-            ]
-        },
-        {
-            id: "3",
-            week: 2,
-            day: 1,
-            description: "Full body workout",
-            exercise: [
-                { id: "5", name: "Deadlift", series: 3, repetitions: 5, weight: 100, rir: 1, rpe: 9, comments: "Challenging" }
-            ]
-        }] as Workout[]
-} as UserWorkout
-
-    
 export const UserWorkoutContainer = () => {
     const location = useLocation();
     const { loading, callEndpoint } = useService<GetUserWorkoutResponse>();
+    const router = useRouter();
 
-    const [userWorkout, setUserWorkout] = useState<UserWorkout>(mockedUserWorkout);
+    const [userWorkout, setUserWorkout] = useState<UserWorkout | null>(null);
 
     const fetchUserWorkout = async (id: string) => {
         const response = await callEndpoint(await workoutService.getUserWorkout(id));
-        if (response.data) { 
+        if (response.data) {
             setUserWorkout(adaptGetUserWorkoutToUserWorkout(response.data));
         }
+    }
+
+    const handleGetWorkoutHistoric = async () => {
+        router.push(paths.users.workoutHistory, {
+            state: {
+                userId: location.state?.userId
+            }
+        })
     }
 
     useEffect(() => {
@@ -82,6 +50,18 @@ export const UserWorkoutContainer = () => {
                 <LinearProgress sx={{ mt: 5 }} />
             </Box>
         );
+
+    if (!userWorkout) {
+        return (
+            <Paper sx={{ width: '100%', p: 2, height: '600px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <Stack spacing={2}>
+                    <Typography variant="h4" gutterBottom>
+                        No se encontró plan de entrenamiento del usuario
+                    </Typography>
+                </Stack>
+            </Paper>
+        );
+    }
 
     return (
         <Paper sx={{ width: '100%', p: 2 }}>
@@ -114,7 +94,7 @@ export const UserWorkoutContainer = () => {
                         <span style={{ fontWeight: "bold" }}>{userWorkout.duration} semanas</span>
                     </Typography>
                 </Stack>
-                <Stack direction={{xs: "column", md: "row"}} spacing={{xs: 1, md: 4}} >
+                <Stack direction={{ xs: "column", md: "row" }} spacing={{ xs: 1, md: 4 }} justifyContent={'space-between'} >
                     <Stack direction={"row"} spacing={2}>
                         <SvgIcon>
                             <EventAvailableIcon />
@@ -122,15 +102,14 @@ export const UserWorkoutContainer = () => {
                         <Typography gutterBottom>
                             Inicio: <span style={{ fontWeight: "bold" }}>{format(userWorkout.initDate, "dd MMMM yyyy", { locale: es })}</span>
                         </Typography>
-                    </Stack>
-                    <Stack direction={"row"} spacing={2}>
                         <SvgIcon>
                             <EventBusyIcon />
                         </SvgIcon>
                         <Typography gutterBottom>
                             Fin: <span style={{ fontWeight: "bold" }}>{userWorkout.endDate ? format(userWorkout.endDate, "dd MMMM yyyy", { locale: es }) : "-"}</span>
                         </Typography>
-                    </Stack>
+                    </Stack> 
+                    <Button variant="contained" onClick={handleGetWorkoutHistoric}>Ver Historial</Button>
                 </Stack>
                 <UserWorkoutWeek workouts={userWorkout.workouts} />
             </Stack>
